7546486d346c87366498de9914105b52b5e263f7
Minhaj Sadik <MinhajoSadik@gmail.comm>
2021-07-20 04:12:49 +0600

    [add] some code

--- js/app.js
+++ js/app.js
@@ -42,26 +42,26 @@ $(document).ready(function(){
 
     }
 
-        // let counterSection = document.querySelectorAll('.counter_section');
+        let counterSection = document.querySelectorAll('.counter_section');
         // console.log(counterSection);
 
-        // const options = {
-        //     rootMargin: '0px 0px -200px 0px'
-        // }
+        const options = {
+            rootMargin: '0px 0px -200px 0px'
+        }
 
-        // let done = 0;
+        let done = 0;
 
-        // const sectionObserver = new IntersectionObserver(function(entries){
+        const sectionObserver = new IntersectionObserver(function(entries){
 
-        //     if(entries[0].isIntersecting && done !== 1){
-        //         console.log('intersecting...');
-        //         done = 1;
+            if(entries[0].isIntersecting && done !== 1){
+                // console.log('intersecting...');
+                done = 1;
                 runCounter();
-        //     }
+            }
 
-        // }, options)
+        }, options)
 
-        // sectionObserver.observe(counterSection);
+        sectionObserver.observe(counterSection);
 
         
 
