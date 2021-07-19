c559f91f05cbdd8a81a8c64ce2e3c0ec83b7fcf0
Minhaj Sadik <MinhajoSadik@gmail.comm>
2021-07-19 22:33:08 +0600

    working on

--- js/app.js
+++ js/app.js
@@ -69,7 +69,45 @@ $(document).ready(function(){
         // image filter
 
         let $wrapper = $('.portfolio_wrapper');
-        
+
+        // initialize isotope
+
+        $wrapper.isotope({
+            filter:'*',
+            layoutMode: 'masonry',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear'
+            }
+        });
+
+        let links = document.querySelectorAll('.tabs a');
+
+        links.forEach(link=>{
+
+            let selector = link.dataset.filter;
+
+            link.addEventListener('click', function(e){
+                e.preventDefault();
+
+                $wrapper.isotope({
+                    filter:selector,
+                    layoutMode: 'masonry',
+                    animationOptions: {
+                        duration: 750,
+                        easing: 'linear'
+                    }
+                });
+
+                links.forEach(link=>{
+                    link.classList.remove('active')
+                })
+
+                e.target.classList.add('active');
+
+            });
+        })
+
 });
 
 
